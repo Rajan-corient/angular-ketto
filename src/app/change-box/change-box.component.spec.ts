46c24b83e19edc22d74afd5a190d01c24c3b707d
Rajan Gupta <guptarajan698@gmaail.com>
2021-07-02 00:24:21 +0530

    updated to angular11 and skipped version 10

--- src/app/change-box/change-box.component.spec.ts
+++ src/app/change-box/change-box.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ChangeBoxComponent } from './change-box.component';
 
@@ -6,7 +6,7 @@ describe('ChangeBoxComponent', () => {
   let component: ChangeBoxComponent;
   let fixture: ComponentFixture<ChangeBoxComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ChangeBoxComponent ]
     })
